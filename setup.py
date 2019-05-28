8f3fc737bfc9e0b765890f0ca3c6ce9a84d30f9f
Nicholas Sokolov <sokolov.nicholas@gmail.com>
2019-05-28 21:57:44 +0700

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -21,24 +21,25 @@ def read(*names, **kwargs):
 setup(
     name='xlsx_handler',
     version='0.0.1',
-    license='BSD 2-Clause License',
-    description='Handle for XLSX/XLSM files',
-    long_description='%s\n' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md'))
-    ),
+    # license='BSD 2-Clause License',
+    # description='Handle for XLSX/XLSM files',
+    # long_description='%s\n' % (
+    #     re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md'))
+    # ),
     author='Sokolov Nikolay Valerevich',
     author_email='sokolov.nicholas@gmail.com',
-    url='https://github.com/nicholas-sokolov/xlsx_handler',
-    packages=find_packages('src'),
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
+    # url='https://github.com/nicholas-sokolov/xlsx_handler',
+    # packages=find_packages('src'),
+    packages=['src',],
+    # zip_safe=False,
+    # classifiers=[
+    #     'Development Status :: 5 - Production/Stable',
+    #     'Intended Audience :: Developers',
+    #     'License :: OSI Approved :: BSD License',
+    #     'Operating System :: Unix',
+    #     'Operating System :: POSIX',
+    #     'Operating System :: Microsoft :: Windows',
+    #     'Programming Language :: Python :: 3.6',
+    #     'Programming Language :: Python :: 3.7',
+    # ],
 )
